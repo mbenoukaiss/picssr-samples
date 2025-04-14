e683142aea7dfdfd2817295328f424176d9d39f9
Marwane <mbenoukaiss@gmail.com>
2025-04-14 15:41:59 +0200

    Add missing samples

--- formatter.sh
+++ formatter.sh
@@ -4,13 +4,13 @@ round() {
   printf "%.${2}f" "${1}"
 }
 
-for FILE in samples/*.heic; do
+for FILE in $(find ./original -iname "*.heic"); do
     echo $filename
     LAT=$(round $(exiftool -n -p '$GPSLatitude' $FILE) 5)
     LON=$(round $(exiftool -n -p '$GPSLongitude' $FILE) 5)
 
-    NAME="formatted/sample;${LAT/./,};${LON/./,}.jpg"
-echo $NAME
+    NAME="samples/sample;${LAT/./,};${LON/./,}.jpg"
+
     sips -s format jpeg -s formatOptions best $FILE --out $NAME
 done
 
